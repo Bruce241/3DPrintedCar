4ba7695933b2097677152e94a38ff3e69caf24f7
Bruce241 <brucejr241@gmail.com>
2021-10-17 13:31:25 -0400

    starter code

---
+++ 3dprintedcar/src/main.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+
+//Runs at start of program
+void initialize() {
+
+}
+
+//For autonomous movement
+void autonomous() {
+
+}
+
+//For driver movement
+void opcontrol() {
+
+}
+
+
+void disabled() {}
+void competition_initialize() {}
